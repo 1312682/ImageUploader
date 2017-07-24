c1dd758c55989912bab0cd752cf5bfb80fbd997c
Anh Tú Võ <vatu.mu@gmail.com>
2017-07-24 22:58:22 +0700

    update user helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,13 +1,8 @@
 module UsersHelper
-
   # Returns the Gravatar for the given user.
   def gravatar_for(user, size: 80)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.username, class: 'gravatar')
-  end
-
-  def find_user_by_id id
-    User.find_by(id: id)
   end
 end
